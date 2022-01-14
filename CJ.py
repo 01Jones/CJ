8bda4fab305c6ac3751409cc50adc32666dfca0d
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-01-14 07:50:02 -0600

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -14,6 +14,7 @@ option = st.sidebar.selectbox("Dashboard", ('Home', 'Economic KPI Analysis', 'Di
 if option == 'Home':
     st.title("Portfolio")
     st.header("Collin Jones")
+    background-image: url("https://gfycat.com/agonizingobvioushochstettersfrog-network-loop-background-animation-background")
 
 #pages
 
