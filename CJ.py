d03dc44694d71aa9585c07507dfac70c012cae5f
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-04-12 20:53:46 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -18,11 +18,11 @@ if option == 'Home':
     st.text(" ")
     st.text(" < Navigation with left sidebar")
     st.text("Asset Valuations, Performance Visualization, Digital Asset Tracker, Page 4, Framework")
-    st.text(" ")
-    st.text(" ")
-    st.text(" ")
-    st.text(" ")
-    st.text(" ")
+    st.text(" 
+            
+            
+            ")
+  
             
             
     st.text('Live Streams')
