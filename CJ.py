288d384a00e8ce083853ee2ee4793b996e85c2cf
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-04-13 08:18:21 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -21,16 +21,38 @@ if option == 'Home':
     st.text(" ")
   
             
-    arc_layer = pdk.Layer(
-     "ArcLayer",
-      get_width="S000 * 2",
-      get_source_position=["lng_h", "lat_h"],
-      get_target_position=["lng_w", "lat_w"],
-      get_tilt=15,
-        pickable=True,
-        auto_highlight=True,
-)
-    view_state = pdk.ViewState(latitude=37.7576171, longitude=-122.5776844, bearing=45, pitch=50, zoom=8,)
+ df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+     map_style='mapbox://styles/mapbox/light-v9',
+     initial_view_state=pdk.ViewState(
+         latitude=37.76,
+         longitude=-122.4,
+         zoom=11,
+         pitch=50,
+     ),
+     layers=[
+         pdk.Layer(
+            'HexagonLayer',
+            data=df,
+            get_position='[lon, lat]',
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+         ),
+         pdk.Layer(
+             'ScatterplotLayer',
+             data=df,
+             get_position='[lon, lat]',
+             get_color='[200, 30, 0, 160]',
+             get_radius=200,
+         ),
+     ],
+ ))
     
             
     st.text('Live Streams')
