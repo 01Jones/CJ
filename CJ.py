3b6e1084e61f237236a2dee9c37a9c111e4ad455
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-04-14 10:01:30 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -22,7 +22,14 @@ if option == 'Home':
     st.text(" ")
     st.text(" ")
     st.text(" ")
-  
+    
+    #weather
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Temperature", "70 °F", "1.2 °F")
+    col2.metric("Wind", "9 mph", "-8%")
+    col3.metric("Humidity", "86%", "4%")
+ 
+    #map
     st.pydeck_chart(pdk.Deck(
          map_style='mapbox://styles/mapbox/dark-v8',
          initial_view_state=pdk.ViewState(
