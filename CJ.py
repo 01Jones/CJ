6ecfaa9a628c2f2f2a2bd093fb5a332fee55ff2f
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-04-18 17:05:16 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -32,10 +32,11 @@ if option == 'Home':
     response = requests.get(url)
     data = json.loads(response.text)
     current = data["current"]["temp"]
+    w = data["current"]["wind_speed"]
     
     col1, col2, col3 = st.columns(3)
     col1.metric("Temperature", current, "1.2 °F")
-    col2.metric("Wind", "9 mph", "-8%")
+    col2.metric("Wind", w, "-8%mph")
     col3.metric("Humidity", "86%", "4%")
     
    
