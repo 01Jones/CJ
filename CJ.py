93ca2ee8fbccd480f6658efb3ec5bd86285ed330
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-04-19 12:08:06 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -92,7 +92,8 @@ if option == 'Performance' :
     
     from fredapi import Fred
     fred = Fred(api_key='49dc69fb7e224d27e8cd2f5b4830ac9f')
-    data = fred.get_series('SP500')
+    data = fred.get_series_latest_release('GDP')
+    data.tail()
     
     
         
