ae52d56bdfd60ec60e37f4d3912c475c24c15b70
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-04-25 21:59:22 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -101,6 +101,11 @@ if option == 'Performance' :
     fred = Fred(api_key='49dc69fb7e224d27e8cd2f5b4830ac9f')
     gdp = fred.get_series_latest_release('GDP')
     st.write(gdp)
+    pd.Series(data = gdp, index = label_date)
+    
+    
+    
+    
     st.line_chart(gdp)
     
     
