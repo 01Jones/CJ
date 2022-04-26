f30ac912d0e5e836505f57ae95f66cfbf9a0f0cc
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-04-26 13:20:20 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -20,6 +20,8 @@ if option == 'Home':
     st.header("Project")
     st.text(" ")
     st.text(" < Navigation with left sidebar")
+    st.text(" Framework & Code on Page 6")
+    st.text(" ")
     st.text("Asset Valuations, Performance Visualization, Digital Asset Tracker, Page 4, Framework")
     st.text(" ")
     st.text(" ")
