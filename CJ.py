cbba9daf9ddf5929cdd4cd9d19233dc285ab011a
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-04-26 14:53:20 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -105,17 +105,16 @@ if option == 'Performance' :
     
     from fredapi import Fred
     fred = Fred(api_key='49dc69fb7e224d27e8cd2f5b4830ac9f')
+   
     
-    gdp = fred.get_series_latest_release('GDP')
-    
-    will = fred.get_series_latest_release('WILL5000INDFC')
+    will = fred.get_series_as_of_date('WILL5000INDFC', 'today')
     will.name = 'will'
     
-    cur = fred.get_series_latest_release('WCURCIR')
+    cur = fred.get_series_as_of_date('WCURCIR', 'today')
     cur.name = 'cur'
     
-    a = fred.get_series_as_of_date('GDP', 'today')
-    st.write(a)
+    gdp = fred.get_series_as_of_date('GDP', 'today')
+    st.write(gdp)
     
     st.write(gdp)
     st.write(will)
