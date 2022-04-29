9902488c46230806e10483cf89a26d853518cb47
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-04-29 07:58:15 -0600

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -118,6 +118,7 @@ if option == 'Performance' :
    
     cur = fred.get_series('WCURCIR', observation_start='2014-01-01', observation_end='today')
     gdp = fred.get_series('GDP', observation_start='2014-01-01', observation_end='today')
+    st.line_chart(gdp)
     
     
     
