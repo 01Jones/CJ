21f0977847f56d93555e68c3a72cfbd15cb2a4a6
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-05-10 11:58:00 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -106,7 +106,7 @@ if option == 'Performance' :
     st.text('M1 Money Supply To Total Public Debt'
     
     
-    
+    from fredapi import Fred
     fred = Fred(api_key='49dc69fb7e224d27e8cd2f5b4830ac9f')
    
     start = ('2010-01-01')
