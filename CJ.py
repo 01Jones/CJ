2e0a2641958ccbe6c2769d548c8233950e601dc9
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-05-17 11:28:10 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -94,8 +94,8 @@ if option == 'Valuation Models':
     a4.number_input('Year 4', None, None, 40)
     a5.number_input('Year 5', None, None, 50)
     
-    sum = a0+a1+a2+a3
-    st.text('sum')
+    sum = 'a1'+'a2'+'a3'
+    st.write('sum')
  
     
     
