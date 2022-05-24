6479757984b73acf260b3de2c1a05479ebd86c06
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-05-24 13:06:26 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -171,7 +171,7 @@ if option == 'Performance' :
    
     
     st.text('Currency in Circulation | Unemployment')        
-    st,line_chart(econ)
+    st.line_chart(econ)
     st.write(econ)
     
    
