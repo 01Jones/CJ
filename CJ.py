505b70778dd39bfc0dd7184bbf3a18375269c628
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-06-09 09:30:35 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -205,7 +205,7 @@ if option == 'SEC Document Analysis' :
     
     tik = st.text_input('Company Ticker', 'AAPL')
     
-    st.subheader('tik', 'Recent 8k Filings')
+    st.subheader('Recent 8k Filings')
     
     start = pd.to_datetime('2018-01-01')
     end = pd.to_datetime('today')
