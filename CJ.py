c3ab6194ffde27548210584f359794fb5e3ee7d6
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-06-09 11:03:49 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -264,8 +264,9 @@ if option == 'Company Insights' :
     qf = pd.DataFrame(stock.quarterly_financials)
     qf.dropna(inplace=True)
     st.write(qf)
-
     
+      st.markdown("""---""")
+
     st.write('Largest Share Holders')
     stock.major_holders
    
