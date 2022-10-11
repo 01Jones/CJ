83142f1e039d60b801a78d1bbc21e23bbccef36e
C.Jones <94568312+01Jones@users.noreply.github.com>
2022-10-11 09:17:06 -0500

    Update CJ.py

--- CJ.py
+++ CJ.py
@@ -57,7 +57,7 @@ if option == 'Home':
     
     
     te.login()
-    cal = te.getCalendarData(category='inflation rate', output_type='df')
+    cal = te.getCalendarData(category='inflation rate', importance='2',  output_type='df')
     st.write(cal)
 
 
